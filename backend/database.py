1738a16c13e89829bed148e8dda4d5029bb53806
newtps <chaudharitejalks@gmail.com>
2026-05-06 08:48:35 +0530

    db change

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,15 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "mysql+pymysql://root:TEJAL1234@localhost:3306/dedup_db"
-
-# engine = create_engine(DATABASE_URL)
-
-# SessionLocal = sessionmaker(
-#     autocommit=False,
-#     autoflush=False,
-#     bind=engine
-# )
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:TEJAL1234@localhost:3306/dedup_db"
+# For local/EC2 Simple deployment use SQLite
+DATABASE_URL = "sqlite:///./dedup_system.db"
 
-engine = create_engine(DATABASE_URL)
+# For AWS RDS MySQL, use this format instead:
+# DATABASE_URL = "mysql+pymysql://root:PASSWORD@ENDPOINT:3306/dedup_db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
